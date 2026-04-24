d7eced17beda23839011dd3a8ffdc4a081a89f33
tory hebert <toryhebert@pop-os.attlocal.net>
2026-04-24 10:09:26 -0500

    feat(frontend): create placeholder layout components

---
+++ frontend/src/components/PersonaLayout.tsx
@@ -0,0 +1,8 @@
+// In frontend/src/components/PersonaLayout.tsx
+import React from 'react';
+
+const PersonaLayout: React.FC = () => {
+  return <div>Persona Layout (Coming Soon)</div>;
+};
+
+export default PersonaLayout;
